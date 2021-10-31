44f7643ce39229425bd3e730a531792e7da08ae4
LCypherus <75752265+LCypherus@users.noreply.github.com>
2021-10-31 08:37:26 +0100

    Setting up players bot

---
+++ commands/get-invitelink.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const commandInvitelinkSchema = require('@schemas/command-invitelink-schema')
+
+module.exports = {
+    commands: ['invitelink', 'get-invitelink'],
+    slash: 'both',  
+    testOnly: true,
+    
+    category: 'Player Commands',
+    description: 'Receive this server\'s invite link.',
+
+    // minArgs: 0,
+    // maxArgs: 0,
+    
+    callback: async ({ guild }) => {
+        const guildId = guild.id
+        const result = await commandInvitelinkSchema.findOne({ _id: guildId })
+        const invitelink = result.invitelink
+
+        return `https://discord.gg/${invitelink}`
+    }
+}
